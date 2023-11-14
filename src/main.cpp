1e5cdfbffb52705e03c93f2434bcb844f7e75d9e
ericssonl07 <ljh07ericsson@gmail.com>
2023-11-14 16:34:28 +0800

    Commit 1.1: Tested Odometry code, corrected doubling, still need to figure out source of doubling

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,37 @@
 #include <thread>
 #include <vex.h>
 
-vex::motor left_motor_1_top(vex::PORT1, vex::gearSetting::ratio6_1, true);
-vex::motor left_motor_1_bottom(vex::PORT2, vex::gearSetting::ratio6_1, false);
-vex::motor left_motor_2(vex::PORT3, vex::gearSetting::ratio6_1, false);
-vex::motor right_motor_1_top(vex::PORT4, vex::gearSetting::ratio6_1, false);
-vex::motor right_motor_1_bottom(vex::PORT5, vex::gearSetting::ratio6_1, true);
-vex::motor right_motor_2(vex::PORT6, vex::gearSetting::ratio6_1, true);
-vex::motor_group left(left_motor_1_top, left_motor_1_bottom, left_motor_2);
-vex::motor_group right(right_motor_1_top, right_motor_1_bottom, right_motor_2);
+vex::motor left_motor_1(vex::PORT3, vex::gearSetting::ratio6_1, true);
+vex::motor left_motor_2_top(vex::PORT1, vex::gearSetting::ratio6_1, false);
+vex::motor left_motor_2_bottom(vex::PORT2, vex::gearSetting::ratio6_1, true);
+
+vex::motor right_motor_1(vex::PORT6, vex::gearSetting::ratio6_1, false);
+vex::motor right_motor_2_top(vex::PORT4, vex::gearSetting::ratio6_1, true);
+vex::motor right_motor_2_bottom(vex::PORT5, vex::gearSetting::ratio6_1, false);
+
+vex::motor_group left(left_motor_2_top, left_motor_2_bottom, left_motor_1);
+vex::motor_group right(right_motor_2_top, right_motor_2_bottom, right_motor_1);
 vex::brain brain;
 vex::controller controller;
 
-#define BASE_WIDTH 27.0 // ensure consistent units
-#define WHEEL_DIAMETER 3.0 // ensure consistent units
+#define BASE_WIDTH 31.7 // centimeters
+#define WHEEL_DIAMETER 8.3 // centimeters
 
 double x_position, y_position, rotation_value;
 double x() { return x_position; }
 double y() { return y_position; }
-double rotation() { return to_deg(rotation_value); }
 double to_rad(double degree_measure);
 double to_deg(double radian_measure);
+double rotation() { return to_deg(rotation_value); }
 void track(void);
+void pos_read(void);
 
 int main(int argc, const char * argv[]) {
     vex::thread tracking(track);
+    while (true) {
+        brain.Screen.print("%.2lf %.2lf %.2lf", x(), y(), rotation());
+    }
     tracking.join();
-    return 0;
 }
 
 
@@ -49,8 +54,8 @@ void track(void) {
     double delta_left_position, delta_right_position;
     double delta_rotation;
     double center_radius, local_x_translation, local_offset;
-    auto get_left = [] () -> double { return (left_motor_1_bottom.position(vex::deg) + left_motor_2.position(vex::deg)) * 0.5 * degree_radian * wheel_diameter; };
-    auto get_right = [] () -> double { return (right_motor_1_bottom.position(vex::deg) + right_motor_2.position(vex::deg)) * 0.5 * degree_radian * wheel_diameter; };
+    auto get_left = [] () -> double { return (left_motor_2_bottom.position(vex::deg) + left_motor_1.position(vex::deg)) * 0.25 * degree_radian * wheel_diameter; }; // factor halved (unsure on source of doubling currently)
+    auto get_right = [] () -> double { return (right_motor_2_bottom.position(vex::deg) + right_motor_1.position(vex::deg)) * 0.25 * degree_radian * wheel_diameter; }; // factor halved
     x_position = 0.0;
     y_position = 0.0;
     rotation_value = 0.0;
@@ -74,6 +79,11 @@ void track(void) {
         x_position += local_x_translation * cos(local_offset);
         y_position += local_x_translation * sin(local_offset);
         rotation_value += delta_rotation;
+    }
+}
+void pos_read(void) {
+    while (true) {
+        vexDelay(2000);
         std::cout << x() << ' ' << y() << ' ' << rotation() << '\n';
         brain.Screen.print("%.2lf %.2lf %.2lf", x(), y(), rotation());
     }
